83292bd8a0444a530b4f9be700938955082f7958
MechCoder <manojkumarsivaraj334@gmail.com>
2016-03-22 17:35:35 -0400

    Make travis to work

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name='skopt',
+      version='0.1',
+      description='Black box optimization-techniques',
+      packages=['distutils', 'distutils.command'],
+     )
---
+++ skopt/__init__.py
@@ -0,0 +1,1 @@
+from .gp_opt import gp_minimize
