1944c49e0f55e6b968052629b641e9815ac18ed8
MechCoder <manojkumarsivaraj334@gmail.com>
2016-03-28 16:45:57 -0400

    Add dummy search method

---
+++ skopt/tests/test_dummy_search.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+from skopt import dummy_search
+from skopt.tests.test_gp_opt import branin
+
+from sklearn.utils.testing import assert_array_equal
+from sklearn.utils.testing import assert_array_less
+
+def test_dummy_search_api():
+    res = dummy_search(
+        branin, [[-5, 10], [0, 15]], random_state=0, maxiter=100)
+    assert_array_equal(res.x.shape, (2,))
+    assert_array_equal(res.x_iters.shape, (100, 2))
+    assert_array_equal(res.func_vals.shape, (100,))
+    assert_array_less(res.x_iters, np.tile([10, 15], (100, 1)))
+    assert_array_less(np.tile([-5, 0], (100, 1)), res.x_iters)
