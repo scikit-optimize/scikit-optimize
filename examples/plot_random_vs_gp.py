97f4cedd0caf8b70b0688c939aac825662a99b8b
MechCoder <manojkumarsivaraj334@gmail.com>
2016-04-07 17:22:59 -0400

    Remove comparison with dummy_search

--- examples/plot_random_vs_gp.py
+++ examples/plot_random_vs_gp.py
@@ -1,15 +1,23 @@
+"""
+==========================================
+Tying a sklearn estimator with gp_minimize
+==========================================
+
+This example shows how to chain gp_minimize with RandomForestClassifier
+in this case.
+"""
+print(__doc__)
 from time import time
 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import randint as sp_randint
 
 from sklearn.base import clone
 from sklearn.datasets import load_digits
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import GridSearchCV
-from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import cross_val_score
 
-from skopt.dummy_opt import dummy_minimize
 from skopt.gp_opt import gp_minimize
 
 digits = load_digits()
@@ -17,44 +25,28 @@ X, y = digits.data, digits.target
 rfc = RandomForestClassifier(random_state=10)
 
 def compute_mean_validation_score(forest_params):
+    # Hack to allow integer parameters, since the parameters
+    # sampled internally are uniform in a given range.
     forest_params = [int(param) for param in forest_params]
     max_depth, max_features, mss, msl = forest_params
 
-    params = {
-        'max_depth': [max_depth], 'max_features': [max_features],
-        'min_samples_split': [mss], 'min_samples_leaf': [msl]}
-    gscv = GridSearchCV(rfc, params, n_jobs=-1)
-    gscv.fit(X, y)
-    return -gscv.best_score_
+    rfc.set_params(
+        max_depth=max_depth, max_features=max_features,
+        min_samples_split=mss, min_samples_leaf=msl)
+    return -np.mean(cross_val_score(rfc, X, y, cv=3, n_jobs=-1))
 
 # Bounds inspired by
 # http://scikit-learn.org/dev/auto_examples/model_selection/randomized_search.html#example-model-selection-randomized-search-py
 bounds = [(3, 50), (1, 12), (1, 12), (1, 12)]
-
-print("Doing a random search for the best random forest hyperparameter.")
-t = time()
-dummy_model = dummy_minimize(
-    compute_mean_validation_score, bounds, maxiter=100, random_state=0)
-print(time() - t)
-print("Best score obtained, %0.4f" % -dummy_model.fun)
+best_dummy_scores = np.zeros((5, 100))
+best_gp_scores = np.zeros((5, 100))
 
 print("Doing a gp-based search for the best random forest hyperparameter.")
 t = time()
 gp_model = gp_minimize(
-    compute_mean_validation_score, bounds, maxiter=100, random_state=0,
+    compute_mean_validation_score, bounds, maxiter=100,
+    random_state=0,
     n_start=1
     )
 print(time() - t)
 print("Best score obtained, %0.4f" % -gp_model.fun)
-
-best_dummy_scores = [-np.min(dummy_model.func_vals[:i]) for i in range(1, 101)]
-best_gp_scores = [-np.min(gp_model.func_vals[:i]) for i in range(1, 101)]
-
-plt.title("Best score obtained at every iteration")
-plt.plot(range(1, 101), best_dummy_scores, label="Dummy search")
-plt.plot(range(1, 101), best_gp_scores, label="GP search")
-plt.legend(loc="best")
-plt.xlabel("Number of iterations.")
-plt.ylabel("Mean accuracy score")
-plt.ylim([0.885, 0.920])
-plt.show()
