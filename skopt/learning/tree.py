99fdd405b61ebe132d67f128ff0ebbc11b182025
MechCoder <manojkumarsivaraj334@gmail.com>
2016-07-12 00:03:42 -0700

    Add tests and documentation

--- skopt/learning/tree.py
+++ skopt/learning/tree.py
@@ -1,14 +1,42 @@
+import numpy as np
 from sklearn.utils import check_array
-from sklearn.tree import DecisionTreeRegressor as DTRegressor
+from sklearn.tree import DecisionTreeRegressor as sklearn_DecisionTreeRegressor
 
 
-class DecisionTreeRegressor(DTRegressor):
+class DecisionTreeRegressor(sklearn_DecisionTreeRegressor):
     """
     DecisionTreeRegressor that can return the variance of predictions.
     """
-    def predict(self, X, return_variance=False):
+    def predict(self, X, return_std=False):
+        """
+        Predict continuous output for X.
+
+        Parameters
+        ----------
+        * `X` [array-like, shape=(n_samples, n_features)]:
+            Input data.
+
+        * `return_std` [bool, default False]:
+            Whether or not to return the standard deviation.
+
+        Returns
+        -------
+        * `predictions` [array-like, shape=(n_samples,)]:
+            Predicted values for X. If criterion is set to "mse",
+            this is the mean of the target variables in the leaf node that each
+            data point ends up in.
+
+        * `std` [array-like, shape=(n_samples,)]:
+            Standard deviation of the predicted values for X. If criterion
+            is set to "mse", this is the std of the target variables in the leaf
+            node that each data point ends up in.
+        """
         predictions = super(DecisionTreeRegressor, self).predict(X)
-        if return_variance:
+        if return_std:
+            if self.criterion != "mse":
+                raise ValueError(
+                    "Expected impurity to be 'mse', got %s instead"
+                    % self.criterion)
             leaf_node = self.apply(X)
-            return predictions, self.tree_.impurity[leaf_node]
+            return predictions, np.sqrt(self.tree_.impurity[leaf_node])
         return predictions
