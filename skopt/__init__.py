bd2a2e7acce4fcb74daef8f9845754858eb1cb09
Gilles Louppe <g.louppe@gmail.com>
2016-07-16 08:17:49 +0200

    DOC: 'sequential' model-based optimisation

--- skopt/__init__.py
+++ skopt/__init__.py
@@ -1,7 +1,7 @@
 """
 Scikit-Optimize, or `skopt`, is a simple and efficient library
-for model-based optimization, accessible to everybody and reusable in various
-contexts.
+for sequential model-based optimization, accessible to everybody 
+and reusable in various contexts.
 
 The library is built on top of NumPy, SciPy and Scikit-Learn.
 
