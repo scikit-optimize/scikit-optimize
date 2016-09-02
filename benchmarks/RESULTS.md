dceeb2ca7b49677e3566d0ed590e9ea296abfc7d
MechCoder <mks542@nyu.edu>
2016-09-02 13:35:51 -0700

    Update benchmarks

--- benchmarks/RESULTS.md
+++ benchmarks/RESULTS.md
@@ -3,8 +3,21 @@ These are the results got by optimizing the particular set of benchmark function
 
 ## gp_minimize
 
-
-|Blackbox Function| Valid loss | Best loss |
+|Blackbox Function| Minimum | Best minimum |
 ------------------|------------|-----------|
-|Branin| 0.429 +/- 0.04|0.399
-|Hart6| -3.04 +/- 0.25|-3.299 
+|Branin| 0.426 +/- 0.026 |0.401
+|Hart6| -2.974 +/- 0.149|-3.315
+
+## forest_minimize
+
+|Blackbox Function| Minimum | Best minimum |
+------------------|------------|-----------|
+|Branin| 0.420 +/- 0.028 |0.398
+|Hart6| -3.111 +/- 0.050|-3.192
+
+## gbrt_minimize
+
+|Blackbox Function| Minimum | Best minimum |
+------------------|------------|-----------|
+|Branin| 0.475 +/- 0.112 |0.399
+|Hart6| -3.128 +/- 0.035|-3.179
--- benchmarks/bench_branin.py
+++ benchmarks/bench_branin.py
@@ -1,23 +1,27 @@
+from functools import partial
+
 import numpy as np
 
 from skopt.benchmarks import branin
-from skopt import gp_minimize
+from skopt import gp_minimize, forest_minimize, gbrt_minimize
 
 bounds = [(-5.0, 10.0), (0.0, 15.0)]
 n_calls = 200
+optimizers = [("gp_minimize", gp_minimize), ("forest_minimize", forest_minimize),
+              ("gbrt_minimize", gbrt_minimize)]
 
-# Mean over ten calls to the optimization procdeure.
-branin_results = []
-for random_state in range(10):
-    res = gp_minimize(
-        branin, bounds, n_random_starts=1, random_state=random_state,
-        n_calls=n_calls)
-    branin_results.append(res)
+for name, optimizer in optimizers:
+    print(name)
+    results = []
+    for random_state in range(10):
+        print(random_state)
+        res = optimizer(branin, bounds, random_state=random_state, n_calls=n_calls)
+        results.append(res)
 
-optimal_values = [result.fun for result in branin_results]
-mean_optimum = np.mean(optimal_values)
-std = np.std(optimal_values)
-best = np.min(optimal_values)
-print(mean_optimum)
-print(std)
-print(best)
+    optimal_values = [result.fun for result in results]
+    mean_optimum = np.mean(optimal_values)
+    std = np.std(optimal_values)
+    best = np.min(optimal_values)
+    print("Mean optimum: " + str(mean_optimum))
+    print("Std of optimal values" + str(std))
+    print("Best optima:" + str(best))
