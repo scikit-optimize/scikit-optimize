f84f2a98a34ec78515d406f493908dbd671a1a61
MechCoder <manojkumarsivaraj334@gmail.com>
2016-11-04 00:23:35 -0400

    Add benchmarks for function calls taken to reach minimum

--- benchmarks/bench_branin.py
+++ benchmarks/bench_branin.py
@@ -1,5 +1,4 @@
 from functools import partial
-
 import numpy as np
 
 from skopt.benchmarks import branin
@@ -13,6 +12,9 @@ optimizers = [("gp_minimize", gp_minimize), ("forest_minimize", forest_minimize)
 for name, optimizer in optimizers:
     print(name)
     results = []
+    min_func_calls = []
+    time_ = 0.0
+
     for random_state in range(10):
         print(random_state)
         if name == "gp_minimize":
@@ -23,6 +25,7 @@ for name, optimizer in optimizers:
             res = optimizer(
                 branin, bounds, random_state=random_state, n_calls=n_calls)
         results.append(res)
+        min_func_calls.append(np.argmin(res.func_vals) + 1)
 
     optimal_values = [result.fun for result in results]
     mean_optimum = np.mean(optimal_values)
@@ -31,3 +34,10 @@ for name, optimizer in optimizers:
     print("Mean optimum: " + str(mean_optimum))
     print("Std of optimal values" + str(std))
     print("Best optima:" + str(best))
+
+    mean_fcalls = np.mean(min_func_calls)
+    std_fcalls = np.std(min_func_calls)
+    best_fcalls = np.min(min_func_calls)
+    print("Mean func_calls to reach min: " + str(mean_fcalls))
+    print("Std func_calls to reach min: " + str(std_fcalls))
+    print("Fastest no of func_calls to reach min: " + str(best_fcalls))
