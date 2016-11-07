Fix infinite gradients.

skopt/learning/gaussian_process/kernels.py
@@ -66,7 +66,10 @@ class RBF(Kernel, sk_RBF):
     def gradient_x(self, x, X_train):
         # diff = (x - X) / length_scale
         # size = (n_train_samples, n_dimensions)
-        length_scale = np.array(self.length_scale)
+        x = np.asarray(x)
+        X_train = np.asarray(X_train)
+
+        length_scale = np.asarray(self.length_scale)
         diff = x - X_train
         diff /= length_scale
 
@@ -86,7 +89,9 @@ class RBF(Kernel, sk_RBF):
 
 class Matern(Kernel, sk_Matern):
     def gradient_x(self, x, X_train):
-        length_scale = np.array(self.length_scale)
+        x = np.asarray(x)
+        X_train = np.asarray(X_train)
+        length_scale = np.asarray(self.length_scale)
 
         # diff = (x - X_train) / length_scale
         # size = (n_train_samples, n_dimensions)
@@ -105,11 +110,16 @@ class Matern(Kernel, sk_Matern):
             scaled_exp_dist = -dist
             scaled_exp_dist = np.exp(scaled_exp_dist, scaled_exp_dist)
             scaled_exp_dist *= -1
-            scaled_exp_dist /= dist
-            scaled_exp_dist = np.expand_dims(scaled_exp_dist, axis=1)
 
             # grad = (e * diff) / length_scale
-            gradient = scaled_exp_dist * diff
+            # Limit of (-e^(-d) * d_i) / (sqrt(\sum_{j=1}^D d_j**2))
+            # where (d_1, d_2 ... d_D) = (0.0, 0.0 ... 0.0)
+            # equals -1
+            gradient = -np.ones((X_train.shape[0], x.shape[0]))
+            mask = dist != 0.0
+            scaled_exp_dist[mask] /= dist[mask]
+            scaled_exp_dist = np.expand_dims(scaled_exp_dist, axis=1)
+            gradient[mask] = scaled_exp_dist[mask] * diff[mask]
             gradient /= length_scale
             return gradient
 
@@ -120,7 +130,11 @@ class Matern(Kernel, sk_Matern):
             sqrt_3_dist = sqrt(3) * dist
             f = np.expand_dims(1 + sqrt_3_dist, axis=1)
 
-            dist_expand = np.expand_dims(sqrt(3) / dist, axis=1)
+            # Gradients when dist equal to zero is zero.
+            sqrt_3_by_dist = np.zeros_like(dist)
+            nzd = dist != 0.0
+            sqrt_3_by_dist[nzd] = sqrt(3) / dist[nzd]
+            dist_expand = np.expand_dims(sqrt_3_by_dist, axis=1)
             f_grad = diff / length_scale
             f_grad *= dist_expand
 
@@ -145,7 +159,10 @@ class Matern(Kernel, sk_Matern):
             f2 += 1
             f = np.expand_dims(f2, axis=1)
 
-            dist = np.reciprocal(dist, dist)
+            # Gradients when dist equal to zero is zero.
+            nzd_mask = dist != 0.0
+            nzd = dist[nzd_mask]
+            dist[nzd_mask] = np.reciprocal(nzd, nzd)
             dist *= sqrt(5)
             dist = np.expand_dims(dist, axis=1)
             diff /= length_scale
@@ -163,6 +180,8 @@ class Matern(Kernel, sk_Matern):
 class RationalQuadratic(Kernel, sk_RationalQuadratic):
 
     def gradient_x(self, x, X_train):
+        x = np.asarray(x)
+        X_train = np.asarray(X_train)
         alpha = self.alpha
         length_scale = self.length_scale
 
@@ -187,6 +206,8 @@ class RationalQuadratic(Kernel, sk_RationalQuadratic):
 class ExpSineSquared(Kernel, sk_ExpSineSquared):
 
     def gradient_x(self, x, X_train):
+        x = np.asarray(x)
+        X_train = np.asarray(X_train)
         length_scale = self.length_scale
         periodicity = self.periodicity
 
@@ -200,7 +221,11 @@ class ExpSineSquared(Kernel, sk_ExpSineSquared):
         exp_sine_squared = np.exp(sine_squared)
 
         grad_wrt_exp = -2 * np.sin(2 * pi_by_period) / length_scale**2
-        grad_wrt_theta = np.pi / (periodicity * dist)
+
+        # Gradients when dist equal to zero are set to zero.
+        grad_wrt_theta = np.zeros_like(dist)
+        nzd = dist != 0.0
+        grad_wrt_theta[nzd] = np.pi / (periodicity * dist[nzd])
         return np.expand_dims(
             grad_wrt_theta * exp_sine_squared * grad_wrt_exp, axis=1) * diff
 
@@ -220,6 +245,8 @@ class WhiteKernel(Kernel, sk_WhiteKernel):
 class Exponentiation(Kernel, sk_Exponentiation):
 
     def gradient_x(self, x, X_train):
+        x = np.asarray(x)
+        X_train = np.asarray(X_train)
         expo = self.exponent
         kernel = self.kernel
 
@@ -239,6 +266,8 @@ class Sum(Kernel, sk_Sum):
 class Product(Kernel, sk_Product):
 
     def gradient_x(self, x, X_train):
+        x = np.asarray(x)
+        X_train = np.asarray(X_train)
         X = np.expand_dims(x, axis=0)
         f_ggrad = (
             np.expand_dims(self.k1(x, X_train)[0], axis=1) *
@@ -254,4 +283,4 @@ class Product(Kernel, sk_Product):
 class DotProduct(Kernel, sk_DotProduct):
 
     def gradient_x(self, x, X_train):
-        return X_train
+        return np.asarray(X_train)

skopt/learning/gaussian_process/tests/test_kernels.py
@@ -28,9 +28,6 @@ KERNELS = [
     DotProduct(sigma_0=2.0)
 ]
 
-rng = np.random.RandomState(0)
-X = rng.randn(5)
-Y = rng.randn(10, 5)
 
 def kernel_X_Y(x, y, kernel):
     X = np.expand_dims(x, axis=0)
@@ -44,11 +41,27 @@ def numerical_gradient(X, Y, kernel):
         grad.append(num_grad)
     return np.asarray(grad)
 
-def check_gradient_correctness(kernel):
+def check_gradient_correctness(kernel, X, Y):
     X_grad = kernel.gradient_x(X, Y)
     num_grad = numerical_gradient(X, Y, kernel)
     assert_array_almost_equal(X_grad, num_grad, decimal=3)
 
 def test_gradient_correctness():
+    rng = np.random.RandomState(0)
+    X = rng.randn(5)
+    Y = rng.randn(10, 5)
     for kernel in KERNELS:
-        yield check_gradient_correctness, kernel
+        yield check_gradient_correctness, kernel, X, Y
+
+def test_gradient_finiteness():
+    """
+    When x is the same as X_train, gradients might become undefined because
+    they are divided by d(x, X_train).
+    Check they are equal to numerical gradients at such points.
+    """
+    for random_state in [0, 1]:
+        rng = np.random.RandomState(random_state)
+        X = rng.randn(5).tolist()
+        Y = [X]
+        for kernel in KERNELS:
+            yield check_gradient_correctness, kernel, X, Y