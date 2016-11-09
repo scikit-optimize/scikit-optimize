[MRG] Fix infinite gradients  (#268)
* Fix infinite gradients.

* Fix infinite gradients in std_grad computation

* One sample condition

* Add gradient computation notebook

* Add more inline comments

* np.allclose

* Add more detailed explanation

* zero -> -1

* Document return_grad

skopt/acquisition.py
@@ -129,6 +129,10 @@ def gaussian_pi(X, model, y_opt=0.0, xi=0.01, return_grad=False):
         Controls how much improvement one wants over the previous best
         values. Useful only when ``method`` is set to "EI"
 
+    * `return_grad`: [boolean, optional]:
+        Whether or not to return the grad. Implemented only for the case where
+        ``X`` is a single sample.
+
     Returns
     -------
     * `values`: [array-like, shape=(X.shape[0],)]:
@@ -150,7 +154,7 @@ def gaussian_pi(X, model, y_opt=0.0, xi=0.01, return_grad=False):
     values[mask] = norm.cdf(scaled)
 
     if return_grad:
-        if not np.any(mask):
+        if not np.all(mask):
             return values, np.zeros_like(std_grad)
         # Substitute (y_opt - xi - mu) / sigma = t and apply chain rule.
         # improve_grad is the gradient of t wrt x.
@@ -196,6 +200,10 @@ def gaussian_ei(X, model, y_opt=0.0, xi=0.01, return_grad=False):
         Controls how much improvement one wants over the previous best
         values. Useful only when ``method`` is set to "EI"
 
+    * `return_grad`: [boolean, optional]:
+        Whether or not to return the grad. Implemented only for the case where
+        ``X`` is a single sample.
+
     Returns
     -------
     * `values`: [array-like, shape=(X.shape[0],)]:
@@ -221,7 +229,7 @@ def gaussian_ei(X, model, y_opt=0.0, xi=0.01, return_grad=False):
     values[mask] = exploit + explore
 
     if return_grad:
-        if not np.any(mask):
+        if not np.all(mask):
             return values, np.zeros_like(std_grad)
 
         # Substitute (y_opt - xi - mu) / sigma = t and apply chain rule.

skopt/learning/gaussian_process/gpr.py
@@ -309,7 +309,9 @@ class GaussianProcessRegressor(sk_GaussianProcessRegressor):
 
                 if return_std_grad:
                     # XXX: Cache np.dot(K_trans, K_inv) from above
-                    grad_std = -np.dot(K_trans, np.dot(K_inv, grad))[0] / y_std
+                    grad_std = np.zeros(X.shape[1])
+                    if not np.allclose(y_std, grad_std):
+                        grad_std = -np.dot(K_trans, np.dot(K_inv, grad))[0] / y_std
                     return y_mean, y_std, grad_mean, grad_std
 
                 if return_std:

skopt/learning/gaussian_process/tests/test_kernels.py
@@ -12,43 +12,58 @@ from skopt.learning.gaussian_process.kernels import RBF
 from skopt.learning.gaussian_process.kernels import WhiteKernel
 
 
-length_scale = np.arange(1, 6)
-KERNELS = [
-    RBF(length_scale=length_scale),
-    Matern(length_scale=length_scale, nu=0.5),
-    Matern(length_scale=length_scale, nu=1.5),
-    Matern(length_scale=length_scale, nu=2.5),
-    RationalQuadratic(alpha=2.0, length_scale=2.0),
-    ExpSineSquared(length_scale=2.0, periodicity=3.0),
-    ConstantKernel(constant_value=1.0),
-    WhiteKernel(noise_level=2.0),
-    Matern(length_scale=length_scale, nu=2.5) ** 3.0,
-    RBF(length_scale=length_scale) + Matern(length_scale=length_scale, nu=1.5),
-    RBF(length_scale=length_scale) * Matern(length_scale=length_scale, nu=1.5),
-    DotProduct(sigma_0=2.0)
-]
+KERNELS = []
 
-rng = np.random.RandomState(0)
-X = rng.randn(5)
-Y = rng.randn(10, 5)
+for length_scale in [np.arange(1, 6), [0.2, 0.3, 0.5, 0.6, 0.1]]:
+    KERNELS.extend([
+        RBF(length_scale=length_scale),
+        Matern(length_scale=length_scale, nu=0.5),
+        Matern(length_scale=length_scale, nu=1.5),
+        Matern(length_scale=length_scale, nu=2.5),
+        RationalQuadratic(alpha=2.0, length_scale=2.0),
+        ExpSineSquared(length_scale=2.0, periodicity=3.0),
+        ConstantKernel(constant_value=1.0),
+        WhiteKernel(noise_level=2.0),
+        Matern(length_scale=length_scale, nu=2.5) ** 3.0,
+        RBF(length_scale=length_scale) + Matern(length_scale=length_scale, nu=1.5),
+        RBF(length_scale=length_scale) * Matern(length_scale=length_scale, nu=1.5),
+        DotProduct(sigma_0=2.0)
+    ])
+
 
 def kernel_X_Y(x, y, kernel):
     X = np.expand_dims(x, axis=0)
     Y = np.expand_dims(y, axis=0)
     return kernel(X, Y)[0][0]
 
-def numerical_gradient(X, Y, kernel):
+def numerical_gradient(X, Y, kernel, step_size=1e-4):
     grad = []
     for y in Y:
-        num_grad = optimize.approx_fprime(X, kernel_X_Y, 1e-4, y, kernel)
+        num_grad = optimize.approx_fprime(X, kernel_X_Y, step_size, y, kernel)
         grad.append(num_grad)
     return np.asarray(grad)
 
-def check_gradient_correctness(kernel):
+def check_gradient_correctness(kernel, X, Y, step_size=1e-4):
     X_grad = kernel.gradient_x(X, Y)
-    num_grad = numerical_gradient(X, Y, kernel)
+    num_grad = numerical_gradient(X, Y, kernel, step_size)
     assert_array_almost_equal(X_grad, num_grad, decimal=3)
 
 def test_gradient_correctness():
+    rng = np.random.RandomState(0)
+    X = rng.randn(5)
+    Y = rng.randn(10, 5)
     for kernel in KERNELS:
-        yield check_gradient_correctness, kernel
+        yield check_gradient_correctness, kernel, X, Y
+
+def test_gradient_finiteness():
+    """
+    When x is the same as X_train, gradients might become undefined because
+    they are divided by d(x, X_train).
+    Check they are equal to numerical gradients at such points.
+    """
+    for random_state in [0, 1]:
+        rng = np.random.RandomState(random_state)
+        X = rng.randn(5).tolist()
+        Y = [X]
+        for kernel in KERNELS:
+            yield check_gradient_correctness, kernel, X, Y, 1e-6