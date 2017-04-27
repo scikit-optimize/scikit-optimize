84f17b192c97212c7fdd963208f41085c85f08a5
Iaroslav Shcherbatyi <iaroslav.email@gmail.com>
2017-04-27 10:55:26 +0200

    Drop ThreadPool, use joblib instead

--- examples/constant_liar.py
+++ examples/constant_liar.py
@@ -5,17 +5,19 @@ The points to evaluate in parallel are selected according to the "constant lie" 
 """
 
 import numpy as np
-from multiprocessing.pool import ThreadPool
+from sklearn.externals.joblib import Parallel, delayed
 from skopt.space import Real
 from skopt.learning import GaussianProcessRegressor
 from skopt import Optimizer
 
-# ThreadPool is used for parallel computations
-pool = ThreadPool()
 optimizer = Optimizer(
     base_estimator=GaussianProcessRegressor(),
     dimensions=[Real(-3.0, 3.0) for i in range(10)]
 )
+
+# objective function to minimze
+def objective(x):
+    return np.sum(np.array(x) ** 2)
 
 # configure number of threads to be used in parallel, and overall # of computations
 n_points, n_steps, Y = 4, 20, []
@@ -23,7 +25,7 @@ n_points, n_steps, Y = 4, 20, []
 for i in range(n_steps):
     x = optimizer.ask(n_points)
     # evaluate n_points in parallel
-    y = pool.map(lambda x: np.sum(np.array(x) ** 2), x)
+    y = Parallel()(delayed(objective)(v) for v in x)
     # tell points and corresponding objectives to the optimizer
     optimizer.tell(x, y)
     # keep objectives history
