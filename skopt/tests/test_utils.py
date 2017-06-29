516d37046ad2fe4f27a63a8b2eed057785761faf
Tim Head <betatim@gmail.com>
2017-06-29 16:07:50 +0200

    Prevent gradients from going to zero

--- skopt/tests/test_utils.py
+++ skopt/tests/test_utils.py
@@ -77,14 +77,14 @@ def test_expected_minimum():
     res = gp_minimize(bench3,
                       [(-2.0, 2.0)],
                       x0=[0.],
-                      noise=0.0,
+                      noise=1e-8,
                       n_calls=20,
                       random_state=1)
 
     x_min, f_min = expected_minimum(res, random_state=1)
     x_min2, f_min2 = expected_minimum(res, random_state=1)
 
-    assert f_min <= res.fun  # true since noise=0.0
+    assert f_min <= res.fun  # true since noise ~= 0.0
     assert x_min == x_min2
     assert f_min == f_min2
 
