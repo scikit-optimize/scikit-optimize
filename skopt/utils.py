08b1d9f3cc072c770d552cfee0f53a7ec83eaec4
Todd <myoung44@vols.utk.edu>
2017-07-07 12:00:08 -0400

    Matching spacing in ValueError statements

--- skopt/utils.py
+++ skopt/utils.py
@@ -456,8 +456,8 @@ def normalize_dimensions(dimensions):
     if isinstance(base_estimator, str):
         base_estimator = base_estimator.upper()
         if base_estimator not in ["GP", "ET", "RF", "GBRT"]:
-            raise ValueError("Valid strings for the base_estimator parameter \
-                              are: 'RF', 'ET','GBRT', or 'GP'.")
+            raise ValueError("Valid strings for the base_estimator parameter "
+                   "are: 'RF', 'ET', 'GBRT', or 'GP'.")
     elif not is_regressor(base_estimator):
         raise ValueError("base_estimator has to be a regressor.")
 
