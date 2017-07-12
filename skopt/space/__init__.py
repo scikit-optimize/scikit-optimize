7b431c272964feb5d694c58379069ecde80d3617
Tim Head <betatim@gmail.com>
2017-07-12 21:33:59 +0200

    Restructure the API of skopt.space

--- skopt/space/__init__.py
+++ skopt/space/__init__.py
@@ -1,5 +1,10 @@
 """
-Utilities to define a search space.
+Define the search space of the optimization problem.
 """
 
-from .space import *
+from .space import Categorical
+from .space import Dimension
+from .space import Integer
+from .space import Real
+from .space import Space
+from .space import check_dimension
