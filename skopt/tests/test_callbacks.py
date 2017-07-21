b409d810a6b69622dfd08d3887d716112d2a7bb3
Todd <myoung44@vols.utk.edu>
2017-07-21 01:07:21 -0400

    Adding first tests for DeadlineStopper

--- skopt/tests/test_callbacks.py
+++ skopt/tests/test_callbacks.py
@@ -1,14 +1,18 @@
 import pytest
 
+import numpy as np
 from collections import namedtuple
 
 from sklearn.utils.testing import assert_equal
 from sklearn.utils.testing import assert_less
 
 from skopt import dummy_minimize
+from skopt import gp_minimize
 from skopt.benchmarks import bench1
+from skopt.benchmarks import bench3
 from skopt.callbacks import TimerCallback
 from skopt.callbacks import DeltaYStopper
+from skopt.callbacks import DeadlineStopper
 
 
 @pytest.mark.fast_test
@@ -28,3 +32,16 @@ def test_deltay_stopper():
     assert deltay(Result([0, 1, 2, 3, 4, 0.1, 0.19]))
     assert not deltay(Result([0, 1, 2, 3, 4, 0.1]))
     assert deltay(Result([0, 1])) is None
+
+
+@pytest.mark.fast_test
+def test_deadline_stopper():
+    deadline = DeadlineStopper(0.0001)
+    gp_minimize(bench3, [(-1.0, 1.0)], callback=deadline, n_calls=10, random_state=1)
+    assert len(deadline.iter_time) == 1
+    assert np.sum(deadline.iter_time) > deadline.total_time
+
+    deadline = DeadlineStopper(60)
+    gp_minimize(bench3, [(-1.0, 1.0)], callback=deadline, n_calls=10, random_state=1)
+    assert len(deadline.iter_time) == 10
+    assert np.sum(deadline.iter_time) < deadline.total_time
