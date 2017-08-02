9b3ab183b6271a81e4222131711720a9ad8fd7ae
Tim Head <betatim@gmail.com>
2017-08-02 08:19:35 +0200

    Tweak GBRT docstrings.

--- skopt/optimizer/gbrt.py
+++ skopt/optimizer/gbrt.py
@@ -14,121 +14,140 @@ def gbrt_minimize(func, dimensions, base_estimator=None,
     """Sequential optimization using gradient boosted trees.
 
     Gradient boosted regression trees are used to model the (very)
-    expensive to evaluate function `func`. The model is improved
+    expensive to evaluate function ``func``. The model is improved
     by sequentially evaluating the expensive function at the next
     best point. Thereby finding the minimum of `func` with as
     few evaluations as possible.
 
-    The total number of evaluations, `n_calls`, are performed like the
-    following. If `x0` is provided but not `y0`, then the elements of `x0`
-    are first evaluated, followed by `n_random_starts` evaluations.
-    Finally, `n_calls - len(x0) - n_random_starts` evaluations are
-    made guided by the surrogate model. If `x0` and `y0` are both
-    provided then `n_random_starts` evaluations are first made then
-    `n_calls - n_random_starts` subsequent evaluations are made
+    The total number of evaluations, ``n_calls``, are performed like the
+    following. If ``x0`` is provided but not ``y0``, then the elements of
+    ``x0`` are first evaluated, followed by ``n_random_starts`` evaluations.
+    Finally, ``n_calls - len(x0) - n_random_starts`` evaluations are
+    made guided by the surrogate model. If ``x0`` and ``y0`` are both
+    provided then ``n_random_starts`` evaluations are first made then
+    ``n_calls - n_random_starts`` subsequent evaluations are made
     guided by the surrogate model.
 
     Parameters
     ----------
-    * `func` [callable]:
+    func : callable
         Function to minimize. Should take a array of parameters and
         return the function values.
 
-    * `dimensions` [list, shape=(n_dims,)]:
+    dimensions : list, shape=(n_dims,)
         List of search space dimensions.
         Each search dimension can be defined either as
 
-        - a `(upper_bound, lower_bound)` tuple (for `Real` or `Integer`
+        - a ``(lower_bound, upper_bound)`` tuple (for ``Real`` or ``Integer``
           dimensions),
-        - a `(upper_bound, lower_bound, "prior")` tuple (for `Real`
+        - a ``(lower_bound, upper_bound, "prior")`` tuple (for ``Real``
           dimensions),
-        - as a list of categories (for `Categorical` dimensions), or
-        - an instance of a `Dimension` object (`Real`, `Integer` or
-          `Categorical`).
+        - as a list of categories (for ``Categorical`` dimensions), or
+        - an instance of a ``Dimension`` object (``Real``, ``Integer`` or
+          ``Categorical``).
 
-    * `base_estimator` [`GradientBoostingQuantileRegressor`]:
-        The regressor to use as surrogate model
+         NOTE: The upper and lower bounds are inclusive for ``Integer``
+         dimensions.
 
-    * `n_calls` [int, default=100]:
-        Number of calls to `func`.
+    base_estimator : ``GradientBoostingQuantileRegressor``
+        The regressor to use as surrogate model.
 
-    * `n_random_starts` [int, default=10]:
-        Number of evaluations of `func` with random points before
-        approximating it with `base_estimator`.
+    n_calls : int, optional (default=100)
+        Number of calls to ``func``.
 
-    * `acq_func` [string, default=`"LCB"`]:
+    n_random_starts : int, optional (default=10)
+        Number of evaluations of ``func`` with random points before
+        approximating it with ``base_estimator``.
+
+    acq_func : string, optional (default="LCB")
         Function to minimize over the forest posterior. Can be either
 
-        - `"LCB"` for lower confidence bound.
-        - `"EI"` for negative expected improvement.
-        - `"PI"` for negative probability of improvement.
-        - ``"EIps"`` for negated expected improvement per second to take into
+        - "LCB" for lower confidence bound.
+        - "EI" for negative expected improvement.
+        - "PI" for negative probability of improvement.
+        - "gp_hedge" Probabilistically choose one of the above three
+          acquisition functions at every iteration. The weightage
+          given to these gains can be set by ``\eta`` through
+          ``acq_func_kwargs``.
+            - The gains ``g_i`` are initialized to zero.
+            - At every iteration,
+                - Each acquisition function is optimised independently to
+                  propose an candidate point ``X_i``.
+                - Out of all these candidate points, the next point ``X_best``
+                  is chosen by ``softmax(\eta g_i)``.
+                - After fitting the surrogate model with ``(X_best, y_best)``,
+                  the gains are updated such that ``g_i -= \mu(X_i)``
+        - "EIps" for negated expected improvement per second to take into
           account the function compute time. Then, the objective function is
           assumed to return two values, the first being the objective value and
           the second being the time taken.
-        - `"PIps"` for negated probability of improvement per second.
+        - "PIps" for negated probability of improvement per second.
 
-    * `x0` [list, list of lists or `None`]:
+    x0 : list, list of lists or ``None``
         Initial input points.
 
         - If it is a list of lists, use it as a list of input points.
         - If it is a list, use it as a single initial input point.
-        - If it is `None`, no initial input points are used.
+        - If it is ``None``, no initial input points are used.
 
-    * `y0` [list, scalar or `None`]:
+    y0 : list, scalar or ``None``
         Evaluation of initial input points.
 
         - If it is a list, then it corresponds to evaluations of the function
-          at each element of `x0` : the i-th element of `y0` corresponds
-          to the function evaluated at the i-th element of `x0`.
+          at each element of ``x0`` : the i-th element of ``y0`` corresponds
+          to the function evaluated at the i-th element of ``x0``.
         - If it is a scalar, then it corresponds to the evaluation of the
-          function at `x0`.
-        - If it is None and `x0` is provided, then the function is evaluated
-          at each element of `x0`.
+          function at ``x0``.
+        - If it is None and ``x0`` is provided, then the function is evaluated
+          at each element of ``x0``.
 
-    * `random_state` [int, RandomState instance, or None (default)]:
+    random_state : int, RandomState instance or None, optional (default=None)
         Set random state to something other than None for reproducible
         results.
 
-    * `verbose` [boolean, default=False]:
+    verbose : boolean (default=False)
         Control the verbosity. It is advised to set the verbosity to True
         for long optimization runs.
 
-    * `callback` [callable, optional]
-        If provided, then `callback(res)` is called after call to func.
+    callback : callable, list of callables, optional
+        If callable then ``callback(res)`` is called after each call to
+        ``func``. If list of callables, then each callable in the list is
+        called.
 
-    * `n_points` [int, default=10000]:
-        Number of points to sample when minimizing the acquisition function.
+    n_points : int, optional (default=10000)
+        Number of points to sample to determine the next "best" point.
+        Useless if acq_optimizer is set to ``lbfgs``.
 
-    * `xi` [float, default=0.01]:
-        Controls how much improvement one wants over the previous best
-        values. Used when the acquisition is either `"EI"` or `"PI"`.
-
-    * `kappa` [float, default=1.96]:
+    kappa : float, optional (default=1.96)
         Controls how much of the variance in the predicted values should be
         taken into account. If set to be very high, then we are favouring
         exploration over exploitation and vice versa.
-        Used when the acquisition is `"LCB"`.
+        Used when the acquisition is "LCB".
 
-    * `n_jobs` [int, default=1]:
-        The number of jobs to run in parallel for `fit` and `predict`.
-        If -1, then the number of jobs is set to the number of cores.
+    xi : float, optional (default=0.01)
+        Controls how much improvement one wants over the previous best
+        values. Used when the acquisition is either "EI" or "PI".
+
+    n_jobs : int, optional (default=1)
+        Number of cores used to parallize fitting of ``base_estimator``.
+        Defaults to 1 core. If ``n_jobs=-1``, then number of jobs is set
+        to number of cores.
 
     Returns
     -------
-    * `res` [`OptimizeResult`, scipy object]:
+    res : ``OptimizeResult``, scipy object
         The optimization result returned as a OptimizeResult object.
         Important attributes are:
 
-        - `x` [list]: location of the minimum.
-        - `fun` [float]: function value at the minimum.
-        - `models`: surrogate models used for each iteration.
-        - `x_iters` [list of lists]: location of function evaluation for each
+        - ``x`` [list]: location of the minimum.
+        - ``fun`` [float]: function value at the minimum.
+        - ``models``: surrogate models used for each iteration.
+        - ``x_iters`` [list of lists]: location of function evaluation for each
            iteration.
-        - `func_vals` [array]: function value for each iteration.
-        - `space` [Space]: the optimization space.
-        - `specs` [dict]`: the call specifications.
-        - `rng` [RandomState instance]: State of the random state
+        - ``func_vals`` [array]: function value for each iteration.
+        - ``space`` [Space]: the optimization space.
+        - ``specs`` [dict]: the call specifications.
+        - ``rng`` [RandomState instance]: State of the random state
            at the end of minimization.
 
         For more details related to the OptimizeResult object, refer
