62dddcbd9574fe8b9c4303e7043fa5744f02c3b0
Tim Head <betatim@gmail.com>
2017-08-02 08:10:42 +0200

    Tweak gp_minimize doc string

--- skopt/optimizer/gp.py
+++ skopt/optimizer/gp.py
@@ -31,37 +31,37 @@ def gp_minimize(func, dimensions, base_estimator=None,
     next parameter to evaluate can be made by the acquisition function
     over the Gaussian prior which is much quicker to evaluate.
 
-    The total number of evaluations, `n_calls`, are performed like the
-    following. If `x0` is provided but not `y0`, then the elements of `x0`
-    are first evaluated, followed by `n_random_starts` evaluations.
-    Finally, `n_calls - len(x0) - n_random_starts` evaluations are
-    made guided by the surrogate model. If `x0` and `y0` are both
-    provided then `n_random_starts` evaluations are first made then
-    `n_calls - n_random_starts` subsequent evaluations are made
+    The total number of evaluations, ``n_calls``, are performed like the
+    following. If ``x0`` is provided but not ``y0``, then the elements of ``x0``
+    are first evaluated, followed by ``n_random_starts`` evaluations.
+    Finally, ``n_calls - len(x0) - n_random_starts`` evaluations are
+    made guided by the surrogate model. If ``x0`` and ``y0`` are both
+    provided then ``n_random_starts`` evaluations are first made then
+    ``n_calls - n_random_starts`` subsequent evaluations are made
     guided by the surrogate model.
 
     Parameters
     ----------
-    * `func` [callable]:
+    func : callable
         Function to minimize. Should take a array of parameters and
         return the function values.
 
-    * `dimensions` [list, shape=(n_dims,)]:
+    dimensions : list, shape=(n_dims,)
         List of search space dimensions.
         Each search dimension can be defined either as
 
-        - a `(upper_bound, lower_bound)` tuple (for `Real` or `Integer`
+        - a ``(lower_bound, upper_bound)`` tuple (for ``Real`` or ``Integer``
           dimensions),
-        - a `(upper_bound, lower_bound, "prior")` tuple (for `Real`
+        - a ``(lower_bound, upper_bound, "prior")`` tuple (for ``Real``
           dimensions),
-        - as a list of categories (for `Categorical` dimensions), or
-        - an instance of a `Dimension` object (`Real`, `Integer` or
-          `Categorical`).
+        - as a list of categories (for ``Categorical`` dimensions), or
+        - an instance of a ``Dimension`` object (``Real``, ``Integer`` or
+          ``Categorical``).
 
-         NOTE: The upper and lower bounds are inclusive for `Integer`
+         NOTE: The upper and lower bounds are inclusive for ``Integer``
          dimensions.
 
-    * `base_estimator` [a Gaussian process estimator]:
+    base_estimator : a Gaussian process estimator
         The Gaussian process estimator to use for optimization.
         By default, a Matern kernel is used with the following
         hyperparameters tuned.
@@ -70,106 +70,108 @@ def gp_minimize(func, dimensions, base_estimator=None,
         - Noise that is added to the matern kernel. The noise is assumed
           to be iid gaussian.
 
-    * `n_calls` [int, default=100]:
-        Number of calls to `func`.
+    n_calls : int (default=100)
+        Number of calls to ``func``.
 
-    * `n_random_starts` [int, default=10]:
-        Number of evaluations of `func` with random points before
-        approximating it with `base_estimator`.
+    n_random_starts : int (default=10)
+        Number of evaluations of ``func`` with random points before
+        approximating it with ``base_estimator``.
 
-    * `acq_func` [string, default=`"EI"`]:
+    acq_func : string, (default="EI")
         Function to minimize over the gaussian prior. Can be either
 
-        - `"LCB"` for lower confidence bound.
-        - `"EI"` for negative expected improvement.
-        - `"PI"` for negative probability of improvement.
-        - `"gp_hedge"` Probabilistically choose one of the above three
+        - "LCB" for lower confidence bound.
+        - "EI" for negative expected improvement.
+        - "PI" for negative probability of improvement.
+        - "gp_hedge" Probabilistically choose one of the above three
           acquisition functions at every iteration. The weightage
-          given to these gains can be set by `\eta` through `acq_func_kwargs`.
-            - The gains `g_i` are initialized to zero.
+          given to these gains can be set by ``\eta`` through
+          ``acq_func_kwargs``.
+            - The gains ``g_i`` are initialized to zero.
             - At every iteration,
                 - Each acquisition function is optimised independently to
-                  propose an candidate point `X_i`.
-                - Out of all these candidate points, the next point `X_best` is
-                  chosen by `softmax(\eta g_i)`
-                - After fitting the surrogate model with `(X_best, y_best)`,
-                  the gains are updated such that `g_i -= \mu(X_i)`
-        - `"EIps"` for negated expected improvement per second to take into
+                  propose an candidate point ``X_i``.
+                - Out of all these candidate points, the next point ``X_best``
+                  is chosen by ``softmax(\eta g_i)``.
+                - After fitting the surrogate model with ``(X_best, y_best)``,
+                  the gains are updated such that ``g_i -= \mu(X_i)``
+        - "EIps" for negated expected improvement per second to take into
           account the function compute time. Then, the objective function is
           assumed to return two values, the first being the objective value and
           the second being the time taken in seconds.
-        - `"PIps"` for negated probability of improvement per second. The
+        - "PIps" for negated probability of improvement per second. The
           return type of the objective function is assumed to be similar to
-          that of `"EIps
+          that of "EIps".
 
-    * `acq_optimizer` [string, `"sampling"` or `"lbfgs"`, default=`"lbfgs"`]:
+    acq_optimizer : string, "sampling" or "lbfgs" (default="lbfgs")
         Method to minimize the acquistion function. The fit model
-        is updated with the optimal value obtained by optimizing `acq_func`
-        with `acq_optimizer`.
+        is updated with the optimal value obtained by optimizing ``acq_func``
+        with ``acq_optimizer``.
 
-        The `acq_func` is computed at `n_points` sampled randomly.
+        The ``acq_func`` is computed at ``n_points`` sampled randomly.
 
-        - If set to `"auto"`, then `acq_optimizer` is configured on the
+        - If set to "auto", then ``acq_optimizer`` is configured on the
           basis of the space searched over.
-          If the space is Categorical then this is set to be "sampling"`.
-        - If set to `"sampling"`, then the point among these `n_points`
-          where the `acq_func` is minimum is the next candidate minimum.
-        - If set to `"lbfgs"`, then
-              - The `n_restarts_optimizer` no. of points which the acquisition
+          If the space is ``Categorical`` then this is set to be "sampling".
+        - If set to "sampling", then the point among these ``n_points``
+          where the ``acq_func`` is minimum is the next candidate minimum.
+        - If set to "lbfgs", then
+              - The ``n_restarts_optimizer`` no. of points which the acquisition
                 function is least are taken as start points.
-              - `"lbfgs"` is run for 20 iterations with these points as initial
+              - "lbfgs" is run for 20 iterations with these points as initial
                 points to find local minima.
               - The optimal of these local minima is used to update the prior.
 
-    * `x0` [list, list of lists or `None`]:
+    x0 : list, list of lists or ``None``
         Initial input points.
 
         - If it is a list of lists, use it as a list of input points.
         - If it is a list, use it as a single initial input point.
-        - If it is `None`, no initial input points are used.
+        - If it is ``None``, no initial input points are used.
 
-    * `y0` [list, scalar or `None`]
+    y0 : list, scalar or ``None``
         Evaluation of initial input points.
 
         - If it is a list, then it corresponds to evaluations of the function
-          at each element of `x0` : the i-th element of `y0` corresponds
-          to the function evaluated at the i-th element of `x0`.
+          at each element of ``x0`` : the i-th element of ``y0`` corresponds
+          to the function evaluated at the i-th element of ``x0``.
         - If it is a scalar, then it corresponds to the evaluation of the
-          function at `x0`.
-        - If it is None and `x0` is provided, then the function is evaluated
-          at each element of `x0`.
+          function at ``x0``.
+        - If it is None and ``x0`` is provided, then the function is evaluated
+          at each element of ``x0``.
 
-    * `random_state` [int, RandomState instance, or None (default)]:
+    random_state : int, RandomState instance or None, optional (default=None)
         Set random state to something other than None for reproducible
         results.
 
-    * `verbose` [boolean, default=False]:
+    verbose : boolean (default=False)
         Control the verbosity. It is advised to set the verbosity to True
         for long optimization runs.
 
-    * `callback` [callable, list of callables, optional]
-        If callable then `callback(res)` is called after each call to `func`.
-        If list of callables, then each callable in the list is called.
+    callback : callable, list of callables, optional
+        If callable then ``callback(res)`` is called after each call to
+        ``func``. If list of callables, then each callable in the list is
+        called.
 
-    * `n_points` [int, default=10000]:
+    n_points : int, optional (default=10000)
         Number of points to sample to determine the next "best" point.
-        Useless if acq_optimizer is set to `"lbfgs"`.
+        Useless if acq_optimizer is set to ``lbfgs``.
 
-    * `n_restarts_optimizer` [int, default=5]:
-        The number of restarts of the optimizer when `acq_optimizer`
-        is `"lbfgs"`.
+    n_restarts_optimizer : int, optional (default=5)
+        The number of restarts of the optimizer when ``acq_optimizer``
+        is "lbfgs".
 
-    * `kappa` [float, default=1.96]:
+    kappa : float, optional (default=1.96)
         Controls how much of the variance in the predicted values should be
         taken into account. If set to be very high, then we are favouring
         exploration over exploitation and vice versa.
-        Used when the acquisition is `"LCB"`.
+        Used when the acquisition is "LCB".
 
-    * `xi` [float, default=0.01]:
+    xi : float, optional (default=0.01)
         Controls how much improvement one wants over the previous best
-        values. Used when the acquisition is either `"EI"` or `"PI"`.
+        values. Used when the acquisition is either "EI" or "PI".
 
-    * `noise` [float, default="gaussian"]:
+    noise : float, optional (default="gaussian")
         - Use noise="gaussian" if the objective returns noisy observations.
           The noise of each observation is assumed to be iid with
           mean zero and a fixed variance.
@@ -178,28 +180,28 @@ def gp_minimize(func, dimensions, base_estimator=None,
         - Set this to a value close to zero (1e-10) if the function is
           noise-free. Setting to zero might cause stability issues.
 
-    * `n_jobs` [int, default=1]
+    n_jobs : int, optional (default=1)
         Number of cores to run in parallel while running the lbfgs
         optimizations over the acquisition function. Valid only
-        when `acq_optimizer` is set to "lbfgs."
-        Defaults to 1 core. If `n_jobs=-1`, then number of jobs is set
+        when ``acq_optimizer`` is set to "lbfgs".
+        Defaults to 1 core. If ``n_jobs=-1``, then number of jobs is set
         to number of cores.
 
     Returns
     -------
-    * `res` [`OptimizeResult`, scipy object]:
+    res : ``OptimizeResult``, scipy object
         The optimization result returned as a OptimizeResult object.
         Important attributes are:
 
-        - `x` [list]: location of the minimum.
-        - `fun` [float]: function value at the minimum.
-        - `models`: surrogate models used for each iteration.
-        - `x_iters` [list of lists]: location of function evaluation for each
+        - ``x`` [list]: location of the minimum.
+        - ``fun`` [float]: function value at the minimum.
+        - ``models``: surrogate models used for each iteration.
+        - ``x_iters`` [list of lists]: location of function evaluation for each
            iteration.
-        - `func_vals` [array]: function value for each iteration.
-        - `space` [Space]: the optimization space.
-        - `specs` [dict]`: the call specifications.
-        - `rng` [RandomState instance]: State of the random state
+        - ``func_vals`` [array]: function value for each iteration.
+        - ``space`` [Space]: the optimization space.
+        - ``specs`` [dict]: the call specifications.
+        - ``rng`` [RandomState instance]: State of the random state
            at the end of minimization.
 
         For more details related to the OptimizeResult object, refer
