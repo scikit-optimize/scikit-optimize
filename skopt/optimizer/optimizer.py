c6db46177876a3e898867c5d2856162e68eb65f9
Tim Head <betatim@gmail.com>
2017-08-04 07:39:25 +0200

    Add comment explaining why we sample so many points

--- skopt/optimizer/optimizer.py
+++ skopt/optimizer/optimizer.py
@@ -450,12 +450,11 @@ class Optimizer(object):
                 self.gains_ -= est.predict(np.vstack(self.next_xs_))
             self.models.append(est)
 
-            if self.acq_optimizer == "sampling":
-                X = self.space.transform(self.space.rvs(
-                    n_samples=self.n_points, random_state=self.rng))
-            elif self.acq_optimizer == "lbfgs":
-                X = self.space.transform(self.space.rvs(
-                    n_samples=self.n_restarts_optimizer, random_state=self.rng))
+            # even with BFGS as optimizer we want to sample a large number
+            # of points and then pick the best ones as starting points
+            X = self.space.transform(self.space.rvs(
+                n_samples=self.n_points, random_state=self.rng))
+
             self.next_xs_ = []
             for cand_acq_func in self.cand_acq_funcs_:
                 values = _gaussian_acquisition(
@@ -471,7 +470,7 @@ class Optimizer(object):
                 # minimization starts from `n_restarts_optimizer` different
                 # points and the best minimum is used
                 elif self.acq_optimizer == "lbfgs":
-                    x0 = X[np.argsort(values)]
+                    x0 = X[np.argsort(values)[:self.n_restarts_optimizer]]
 
                     with warnings.catch_warnings():
                         warnings.simplefilter("ignore")
