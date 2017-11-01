d874799134980622e708ac72f371c4696469fa7e
Tim Head <betatim@gmail.com>
2017-11-01 09:31:16 +0100

    Add scikit-garden forest estimators to skopt
    Move the code back into scikit-optimize to reduce the number of
    dependencies of this library. scikit-garden provides mondiran forests
    which require cython and hence a compiler. This adds complexity that we
    do not yet need as we don't use mondrian forests.

--- build_tools/travis/install.sh
+++ build_tools/travis/install.sh
@@ -25,8 +25,7 @@ popd
 
 # Configure the conda environment and put it in the path using the
 # provided versions
-conda create -n testenv --yes python=$PYTHON_VERSION pip nose pytest \
-   numpy
+conda create -n testenv --yes python=$PYTHON_VERSION pip nose pytest
 source activate testenv
 
 if [[ "$COVERAGE" == "true" ]]; then
