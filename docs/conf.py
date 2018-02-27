5baaa0c4e6fe928503f149a2fd51f92d9b8ae4fc
Christopher Schröder <chschroeder1003@gmail.com>
2018-02-27 23:34:06 +0100

    Switch from numpydoc to napoleon

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.mathjax',
               'sphinx.ext.viewcode',
               'sphinx.ext.githubpages',
-              'numpydoc',
+              'sphinx.ext.napoleon'
               ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -83,6 +83,18 @@ pygments_style = 'sphinx'
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
 
+# Napoleon settings
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
 
 # -- Options for HTML output ----------------------------------------------
 
