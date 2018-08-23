fe4ba9e1ba0713f2f37c95db074de5b87d1aae73
Thomas Fan <thomasjpfan@gmail.com>
2018-08-23 12:55:52 -0400

    TST: Convert to circleci 2.0

--- build_tools/circle/install.sh
+++ build_tools/circle/install.sh
@@ -1,34 +1,3 @@
-# Adapted shamelessly from https://github.com/scikit-learn-contrib/project-template/blob/master/ci_scripts/install.sh
-# Deactivate the circleci-provided virtual environment and setup a
-# conda-based environment instead
-if [[ `type -t deactivate` ]]; then
-  deactivate
-fi
-
-# Use the miniconda installer for faster download / install of conda
-# itself
-# XXX: Most of this is very similar to travis/install.sh. We should
-# probably refactor it later.
-pushd .
-cd
-mkdir -p download
-cd download
-echo "Cached in $HOME/download :"
-ls -l
-echo
-if [[ ! -f miniconda.sh ]]
-   then
-   wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh \
-       -O miniconda.sh
-   fi
-chmod +x miniconda.sh && ./miniconda.sh -b
-cd ..
-export PATH="$HOME/miniconda3/bin:$PATH"
-conda update --yes conda
-popd
-
-# Configure the conda environment and put it in the path using the
-# provided versions
 conda create -n testenv --yes python pip pytest nose
 source activate testenv
 
