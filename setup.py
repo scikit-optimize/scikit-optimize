ecf93a9346e7bf3c1fb51adbfe69903d4a350066
Sigurd <sigurdcarlsen@gmail.com>
2019-02-25 17:14:37 +0100

    WIP pip install

--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@ except ImportError:
 import versioneer
 
 
-setup(name='scikit-optimize',
+setup(name='ProcessOptimizer',
       version=versioneer.get_version(),
       cmdclass=versioneer.get_cmdclass(),
-      description='Sequential model-based optimization toolbox.',
+      description='Sequential model-based optimization toolbox (forked from scikit-optimize)',
       long_description=open('README.rst').read(),
-      url='https://scikit-optimize.github.io/',
+      url='https://github.com/bytesandbrains/ProcessOptimizer',
       license='BSD',
-      author='The scikit-optimize contributors',
-      packages=['skopt', 'skopt.learning', 'skopt.optimizer', 'skopt.space',
-                'skopt.learning.gaussian_process'],
+      author='Bytes and Brains',
+      packages=['ProcessOptimizer', 'ProcessOptimizer.learning', 'ProcessOptimizer.optimizer', 'ProcessOptimizer.space',
+                'ProcessOptimizer.learning.gaussian_process'],
       install_requires=['pyaml', 'numpy', 'scipy>=0.14.0',
                         'scikit-learn>=0.19.1'],
       extras_require={
