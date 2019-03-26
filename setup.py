a29798e18ad168c9a2cfc55c44882a5989755ae3
Sigurd Carlsen <sigurdcarlsen@gmail.com>
2019-03-26 13:17:21 +0100

    Change version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
 
 
 setup(name='ProcessOptimizer',
-      version='0.2.0',
+      version='0.2.2',
       description='Sequential model-based optimization toolbox (forked from scikit-optimize)',
       url='https://github.com/bytesandbrains/ProcessOptimizer',
       license='BSD',
