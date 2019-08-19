02fc49e5cfad0fea88e2ef70f5088799d385b72b
sqbl <39369016+sqbl@users.noreply.github.com>
2019-08-19 11:24:37 +0200

    Update setup.py
    Included dependency of 'six' as of the updates by darenr in SKOPT #762

--- setup.py
+++ setup.py
@@ -13,5 +13,5 @@ setup(name='ProcessOptimizer',
       packages=['ProcessOptimizer', 'ProcessOptimizer.learning', 'ProcessOptimizer.optimizer', 'ProcessOptimizer.space',
                 'ProcessOptimizer.learning.gaussian_process'],
       install_requires=['pyaml', 'numpy', 'matplotlib', 'scipy>=0.14.0',
-                        'scikit-learn>=0.19.1', 'bokeh']
+                        'scikit-learn>=0.19.1', 'bokeh', 'six']
       )
