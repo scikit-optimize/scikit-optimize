f76365cf0eb9aa0c25857fd08015aad0beef5763
Sigurd Carlsen <sigurdcarlsen@gmail.com>
2019-10-09 14:43:58 +0200

    Changed optimizer in bokeh_numerical.py

--- plot_test/bokeh_numerical.py
+++ plot_test/bokeh_numerical.py
@@ -1,3 +1,4 @@
+import time
 import matplotlib.pyplot as plt
 from ProcessOptimizer.benchmarks import branin as branin
 from ProcessOptimizer.benchmarks import hart6 as hart6_
@@ -9,21 +10,21 @@ from ProcessOptimizer import bokeh_plot
 import numpy as np
 np.random.seed(123)
 plt.set_cmap("viridis")
-
 # Here we define a function that we evaluate.
 
 
 def funny_func(x):
     s = 0
     for i in range(len(x)):
-        s += (x[i]*i)**2
+        s += (x[i])**2
     return s
 
 
 # We run forest_minimize on the function
 bounds = [(-1, 1.), ] * 7
-n_calls = 40
+n_calls = 30
 
-result = gp_minimize(funny_func, bounds, n_calls=n_calls,
-                     random_state=4)
+result = gp_minimize(funny_func, bounds, n_calls=n_calls, n_random_starts=20,
+                     acq_optimizer="auto", acq_func="gp_hedge", random_state=4)
+
 bokeh_plot.start(result)
