1ae29c5b516ade942dd669d7bda54c11345f5604
holgern <holgernahrstaedt@gmx.de>
2020-01-17 11:38:00 +0100

    Fix typo

--- skopt/tests/test_space.py
+++ skopt/tests/test_space.py
@@ -406,7 +406,7 @@ def test_normalize():
 
     # Check inverse transform
     X_orig = a.inverse_transform(a.transform(X))
-    assert isinstance(X_orig.dtype, np.int64)
+    assert isinstance(X_orig, np.int64)
     assert_array_equal(X_orig, X)
 
 
