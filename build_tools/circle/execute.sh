6a8ec7fc2414a6c4280df76fcda89d6329c1b4e2
holgern <holgernahrstaedt@gmx.de>
2020-01-21 14:17:20 +0100

    Fix build

--- build_tools/circle/execute.sh
+++ build_tools/circle/execute.sh
@@ -19,5 +19,4 @@ mkdir -p ${HOME}/doc/skopt/notebooks
 cp ${SKOPT_HOME}/examples/*md ${HOME}/doc/skopt/notebooks
 find ${SKOPT_HOME}/examples -name \*_files -exec cp -r {} ${HOME}/doc/skopt/notebooks \;
 python ${SKOPT_HOME}/build_tools/circle/make_doc.py --overwrite --html --html-dir ./doc --template-dir ${SKOPT_HOME}/build_tools/circle/templates --notebook-dir ./doc/skopt/notebooks skopt
-cd ${SKOPT_HOME}/docs && make html
-cp -r ${SKOPT_HOME}/docs/build .doc/skopt/rtd
+cd ${SKOPT_HOME}/docs && sphinx-build -M html ${HOME}/doc/skopt/docs/source ${HOME}/doc/skopt/rtd -W --keep-going
