de1a27d2cc259e2f674f808ae52c08722066e061
holgern <holgernahrstaedt@gmx.de>
2020-01-29 10:55:57 +0100

    Fix typo

--- build_tools/circle/install.sh
+++ build_tools/circle/install.sh
@@ -6,7 +6,7 @@ python -m pip install -e '.[plots]'
 export SKOPT_HOME=$(pwd)
 
 conda install --yes jupyter
-pythonp -m pip install pdoc==0.3.2 pygments sphinx sphinx_rtd_theme
+python -m pip install pdoc==0.3.2 pygments sphinx sphinx_rtd_theme
 
 # importing matplotlib once builds the font caches. This avoids
 # having warnings in our example notebooks
