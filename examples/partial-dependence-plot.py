481f9acf1377aeeb896289bcce678f5bfeaae11b
holgern <holgernahrstaedt@gmx.de>
2020-02-04 14:20:14 +0100

    Improve example

--- examples/partial-dependence-plot.py
+++ examples/partial-dependence-plot.py
@@ -49,43 +49,48 @@ result = forest_minimize(funny_func, bounds, n_calls=n_calls, base_estimator="ET
 # predictions for each point on the plots.
 
 
-_ = plot_objective(result, samples='result', n_points=10)
+_ = plot_objective(result, n_points=10)
 
+#############################################################################
 # Here we plot without partial dependence. We see that it is a lot faster.
 # Also the values for the other parameters are set to the default "result"
 # which is the parameter set of the best observed value so far. In the case
 # of funny_func this is close to 0 for all parameters.
 
-_ = plot_objective(result, n_points=10)
+_ = plot_objective(result,  samples='result', n_points=10)
 
-# Here we try with setting the other parameters to something other than
+#############################################################################
+# Here we try with setting the `minimum` parameters to something other than
 # "result". First we try with "expected_minimum" which is the set of
-# parameters that gives the miniumum value of the surogate function,
+# parameters that gives the miniumum value of the surrogate function,
 # using scipys minimum search method.
 
 _ = plot_objective(result, n_points=10,
-                   mininum='expected_minimum')
+                   minimum='expected_minimum')
 
+#############################################################################
 # "expected_minimum_random" is a naive way of finding the minimum of the
-# surogate by only using random sampling:
+# surrogate by only using random sampling:
 
 _ = plot_objective(result, n_points=10,
-                   mininum='expected_minimum_random')
+                   minimum='expected_minimum_random')
 
+#############################################################################
 # Lastly we can also define these parameters ourselfs by parsing a list
 # as the minimum argument:
 
 _ = plot_objective(result, n_points=10, minimum=[1, -0.5, 0.5, 0])
 
+#############################################################################
 # We can also specify how many intial samples are used for the two different
 # "expected_minimum" methods. We set it to a low value in the next examples
 # to showcase how it affects the minimum for the two methods.
 
 _ = plot_objective(result, n_points=10,
-                   mininum='expected_minimum_random',
+                   minimum='expected_minimum_random',
                    expected_minimum_samples=10)
 
 #############################################################################
 
 _ = plot_objective(result, n_points=10,
-                   mininum='expected_minimum', expected_minimum_samples=1)
+                   minimum='expected_minimum', expected_minimum_samples=1)
