41468089224d32151505ac5d8052e9db3c6205bc
holgern <holgernahrstaedt@gmx.de>
2020-02-10 16:15:36 +0100

    Add optimizer helper functions and improve example

--- examples/optimizer-with-different-base-estimator.py
+++ examples/optimizer-with-different-base-estimator.py
@@ -1,7 +1,7 @@
 """
-==========================
-Use unique base estimators
-==========================
+==============================================
+Use different base estimators for optimization
+==============================================
 
 Sigurd Carlen, September 2019.
 Reformatted by Holger Nahrstaedt 2020
@@ -26,109 +26,144 @@ import matplotlib.pyplot as plt
 #
 # Let assume the following noisy function :math:`f`:
 
-noise_level = 0.2
+noise_level = 0.1
 
 # Our 1D toy problem, this is the function we are trying to
 # minimize
-def objective(X, noise_level=noise_level):
-    return -np.sin(3*X[0]) - X[0]**2 + 0.7*X[0] + noise_level * np.random.randn()
 
-#############################################################################
+def objective(x, noise_level=noise_level):
+    return np.sin(5 * x[0]) * (1 - np.tanh(x[0] ** 2))\
+           + np.random.randn() * noise_level
 
-from skopt.learning import GaussianProcessRegressor
-from skopt.learning.gaussian_process.kernels import ConstantKernel, Matern
-# Gaussian process with Matérn kernel as surrogate model
-m52 = ConstantKernel(1.0) * Matern(length_scale=1.0,
-                                   nu=2.5)
-gpr = GaussianProcessRegressor(kernel=m52, alpha=noise_level**2,
-                               normalize_y=True, noise="gaussian",
-                               n_restarts_optimizer=2
-                               )
 #############################################################################
 
 from skopt import Optimizer
-opt = Optimizer([(-1.0, 2.0)], base_estimator=gpr, n_initial_points=5,
-                acq_optimizer="sampling")
+opt_gp = Optimizer([(-2.0, 2.0)], base_estimator="GP", n_initial_points=5,
+                acq_optimizer="sampling", random_state=42)
+
 #############################################################################
 
-x = np.linspace(-1, 2, 400).reshape(-1, 1)
+x = np.linspace(-2, 2, 400).reshape(-1, 1)
 fx = np.array([objective(x_i, noise_level=0.0) for x_i in x])
 
 #############################################################################
 
 from skopt.acquisition import gaussian_ei
 
+def plot_optimizer(res, next_x, x, fx, n_iter, max_iters=5):
+    x_gp = res.space.transform(x.tolist())
+    gp = res.models[-1]
+    curr_x_iters = res.x_iters
+    curr_func_vals = res.func_vals
 
-def plot_optimizer(opt, x, fx):
-    model = opt.models[-1]
-    x_model = opt.space.transform(x.tolist())
     # Plot true function.
+    ax = plt.subplot(max_iters, 2, 2 * n_iter + 1)
     plt.plot(x, fx, "r--", label="True (unknown)")
     plt.fill(np.concatenate([x, x[::-1]]),
              np.concatenate([fx - 1.9600 * noise_level,
                              fx[::-1] + 1.9600 * noise_level]),
              alpha=.2, fc="r", ec="None")
-
-    # Plot Model(x) + contours
-    y_pred, sigma = model.predict(x_model, return_std=True)
-    plt.plot(x, y_pred, "g--", label=r"$\mu(x)$")
+    if n_iter < max_iters - 1:
+        ax.get_xaxis().set_ticklabels([])
+    # Plot GP(x) + contours
+    y_pred, sigma = gp.predict(x_gp, return_std=True)
+    plt.plot(x, y_pred, "g--", label=r"$\mu_{GP}(x)$")
     plt.fill(np.concatenate([x, x[::-1]]),
              np.concatenate([y_pred - 1.9600 * sigma,
                              (y_pred + 1.9600 * sigma)[::-1]]),
              alpha=.2, fc="g", ec="None")
 
     # Plot sampled points
-    plt.plot(opt.Xi, opt.yi,
+    plt.plot(curr_x_iters, curr_func_vals,
              "r.", markersize=8, label="Observations")
+    plt.title(r"x* = %.4f, f(x*) = %.4f" % (res.x[0], res.fun))
+    # Adjust plot layout
+    plt.grid()
 
-    acq = gaussian_ei(x_model, model, y_opt=np.min(opt.yi))
-    # shift down to make a better plot
-    acq = 4 * acq - 2
+    if n_iter == 0:
+        plt.legend(loc="best", prop={'size': 6}, numpoints=1)
+
+    if n_iter != 4:
+        plt.tick_params(axis='x', which='both', bottom='off',
+                        top='off', labelbottom='off')
+
+    # Plot EI(x)
+    ax = plt.subplot(max_iters, 2, 2 * n_iter + 2)
+    acq = gaussian_ei(x_gp, gp, y_opt=np.min(curr_func_vals))
     plt.plot(x, acq, "b", label="EI(x)")
     plt.fill_between(x.ravel(), -2.0, acq.ravel(), alpha=0.3, color='blue')
 
+    if n_iter < max_iters - 1:
+        ax.get_xaxis().set_ticklabels([])
+
+    next_acq = gaussian_ei(res.space.transform([next_x]), gp,
+                           y_opt=np.min(curr_func_vals))
+    plt.plot(next_x, next_acq, "bo", markersize=6, label="Next query point")
+
     # Adjust plot layout
+    plt.ylim(0, 0.07)
     plt.grid()
-    plt.legend(loc='best')
+    if n_iter == 0:
+        plt.legend(loc="best", prop={'size': 6}, numpoints=1)
+
+    if n_iter != 4:
+        plt.tick_params(axis='x', which='both', bottom='off',
+                        top='off', labelbottom='off')
 
 #############################################################################
+# GP kernel
+# ---------
 
+fig = plt.figure()
+fig.suptitle("Standard GP kernel")
 for i in range(10):
-    next_x = opt.ask()
-    print("%.2f next x" % next_x[0])
+    next_x = opt_gp.ask()
     f_val = objective(next_x)
-    r = opt.tell(next_x, f_val)
+    res = opt_gp.tell(next_x, f_val)
     if i >= 5:
-        # plt.subplot(5, 1, i-4)
-        plt.figure()
-        plt.title("%d" % i)
-        plot_optimizer(opt, x, fx)
+        plot_optimizer(res, opt_gp._next_x, x, fx, n_iter=i-5, max_iters=5)
+plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+plt.plot()
 
+#############################################################################
+# Test different kernels
+# ----------------------
+
+from skopt.learning import GaussianProcessRegressor
+from skopt.learning.gaussian_process.kernels import ConstantKernel, Matern
+# Gaussian process with Matérn kernel as surrogate model
+
+from sklearn.gaussian_process.kernels import (RBF, Matern, RationalQuadratic,
+                                              ExpSineSquared, DotProduct,
+                                              ConstantKernel)
+
+
+kernels = [1.0 * RBF(length_scale=1.0, length_scale_bounds=(1e-1, 10.0)),
+           1.0 * RationalQuadratic(length_scale=1.0, alpha=0.1),
+           1.0 * ExpSineSquared(length_scale=1.0, periodicity=3.0,
+                                length_scale_bounds=(0.1, 10.0),
+                                periodicity_bounds=(1.0, 10.0)),
+           ConstantKernel(0.1, (0.01, 10.0))
+               * (DotProduct(sigma_0=1.0, sigma_0_bounds=(0.1, 10.0)) ** 2),
+           1.0 * Matern(length_scale=1.0, length_scale_bounds=(1e-1, 10.0),
+                        nu=2.5)]
 
 #############################################################################
 
-def plot_convergence(X_sample, Y_sample, n_init=5):
-    plt.figure(figsize=(12, 3))
-
-    x = X_sample[n_init:].ravel()
-    y = Y_sample[n_init:].ravel()
-    r = range(1, len(x) + 1)
-
-    x_neighbor_dist = [np.abs(a - b) for a, b in zip(x, x[1:])]
-    y_max_watermark = np.maximum.accumulate(y)
-
-    plt.subplot(1, 2, 1)
-    plt.plot(r[1:], x_neighbor_dist, 'bo-')
-    plt.xlabel('Iteration')
-    plt.ylabel('Distance')
-    plt.title('Distance between consecutive x\'s')
-
-    plt.subplot(1, 2, 2)
-    plt.plot(r, y_max_watermark, 'ro-')
-    plt.xlabel('Iteration')
-    plt.ylabel('Best Y')
-    plt.title('Value of best selected sample')
-
-
-plot_convergence(np.array(r.x_iters), -r.func_vals)
-
+for kernel in kernels:
+    gpr = GaussianProcessRegressor(kernel=kernel, alpha=noise_level ** 2,
+                                   normalize_y=True, noise="gaussian",
+                                   n_restarts_optimizer=2
+                                   )
+    opt = Optimizer([(-2.0, 2.0)], base_estimator=gpr, n_initial_points=5,
+                    acq_optimizer="sampling", random_state=42)
+    fig = plt.figure()
+    fig.suptitle(repr(kernel))
+    for i in range(10):
+        next_x = opt.ask()
+        f_val = objective(next_x)
+        res = opt.tell(next_x, f_val)
+        if i >= 5:
+            plot_optimizer(res, opt._next_x, x, fx, n_iter=i - 5, max_iters=5)
+    plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+    plt.show()
