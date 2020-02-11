c119fbcbeecefb9e58959bf34e4fd45e787bcc37
holgern <holgernahrstaedt@gmx.de>
2020-02-11 22:45:12 +0100

    Fix typo

--- skopt/utils.py
+++ skopt/utils.py
@@ -430,7 +430,7 @@ def dimensions_aslist(search_space):
     ...                 'name2': Integer(2,4), 'name3': Real(-1,1)}
     >>> dimensions_aslist(search_space)[0]
     Real(low=0, high=1, prior='uniform', transform='identity')
-   >>> dimensions_aslist(search_space)[1]
+    >>> dimensions_aslist(search_space)[1]
     Integer(low=2, high=4, prior='uniform', transform='identity')
     >>> dimensions_aslist(search_space)[2]
     Real(low=-1, high=1, prior='uniform', transform='identity')
