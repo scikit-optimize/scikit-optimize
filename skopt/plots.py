dba85f60a70012e16dd951016ad504fb2eb5ea9e
holgern <holgernahrstaedt@gmx.de>
2020-02-26 16:50:13 +0100

    Put back in current code for better merging

--- skopt/plots.py
+++ skopt/plots.py
@@ -563,6 +563,73 @@ def plot_objective_old(result, levels=10, n_points=40, n_samples=250, size=2,
                                      dim_labels=dimensions)
 
 
+def plot_evaluations_old(result, bins=20, dimensions=None):
+    """Visualize the order in which points where sampled.
+
+    The scatter plot matrix shows at which points in the search
+    space and in which order samples were evaluated. Pairwise
+    scatter plots are shown on the off-diagonal for each
+    dimension of the search space. The order in which samples
+    were evaluated is encoded in each point's color.
+    The diagonal shows a histogram of sampled values for each
+    dimension. A red point indicates the found minimum.
+
+    Parameters
+    ----------
+    result : `OptimizeResult`
+        The result for which to create the scatter plot matrix.
+
+    bins : int, bins=20
+        Number of bins to use for histograms on the diagonal.
+
+    dimensions : list of str, default=None
+        Labels of the dimension
+        variables. `None` defaults to `space.dimensions[i].name`, or
+        if also `None` to `['X_0', 'X_1', ..]`.
+
+    Returns
+    -------
+    ax : `Axes`
+        The matplotlib axes.
+    """
+    space = result.space
+    # Convert categoricals to integers, so we can ensure consistent ordering.
+    # Assign indices to categories in the order they appear in the Dimension.
+    # Matplotlib's categorical plotting functions are only present in v 2.1+,
+    # and may order categoricals differently in different plots anyway.
+    samples, minimum, iscat = _map_categories(space, result.x_iters, result.x)
+    order = range(samples.shape[0])
+    fig, ax = plt.subplots(space.n_dims, space.n_dims,
+                           figsize=(2 * space.n_dims, 2 * space.n_dims))
+
+    fig.subplots_adjust(left=0.05, right=0.95, bottom=0.05, top=0.95,
+                        hspace=0.1, wspace=0.1)
+
+    for i in range(space.n_dims):
+        for j in range(space.n_dims):
+            if i == j:
+                if iscat[j]:
+                    bins_ = len(space.dimensions[j].categories)
+                elif space.dimensions[j].prior == 'log-uniform':
+                    low, high = space.bounds[j]
+                    bins_ = np.logspace(np.log10(low), np.log10(high), bins)
+                else:
+                    bins_ = bins
+                ax[i, i].hist(
+                    samples[:, j], bins=bins_,
+                    range=None if iscat[j] else space.dimensions[j].bounds)
+
+            # lower triangle
+            elif i > j:
+                ax[i, j].scatter(samples[:, j], samples[:, i],
+                                 c=order, s=40, lw=0., cmap='viridis')
+                ax[i, j].scatter(minimum[j], minimum[i],
+                                 c=['r'], s=20, lw=0.)
+
+    return _format_scatter_plot_axes(ax, space, ylabel="Number of samples",
+                                     dim_labels=dimensions)
+
+
 def _get_ylim_diagonal(ax):
     """Get the min / max of the ylim for all diagonal plots.
     This is used in _adjust_fig() so the ylim is the same
