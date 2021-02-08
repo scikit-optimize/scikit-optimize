44889c10f4329a9c2c510d2ec23721def2b17490
raziel90 <claudiocoppola90@gmail.com>
2021-02-08 12:06:50 +0000

    removed commented code

--- skopt/acquisition.py
+++ skopt/acquisition.py
@@ -377,11 +377,6 @@ def gaussian_unscented_ei(X, model, y_opt=0.0, xi=0.01, return_grad=False):
     sigma_gen = sigma_points.MerweScaledSigmaPoints(
         n=n_dim, alpha=.3, beta=2., kappa=.1)
     pts = [sigma_gen.sigma_points(xx, cov) for xx in X]
-    # sigma_ei = np.asarray([gaussian_ei(xx, model, y_opt, xi, return_grad) for xx in pts])
-    # sigma_ei = gaussian_ei(
-    #     np.asarray(pts).reshape(-1, 2), 
-    #     model, y_opt, xi, return_grad).reshape(
-    #         X.shape[0], n_sigma, -1)
     grads = []
     eis = []
     for i in range(n_sigma):
@@ -393,23 +388,17 @@ def gaussian_unscented_ei(X, model, y_opt=0.0, xi=0.01, return_grad=False):
             eis.append(res[0])
         else:
             eis.append(res)
-    # sigma_ei = np.asarray([gaussian_ei(
-    #     np.asarray(pts)[:, i, :], 
-    #     model, y_opt, xi, return_grad) for i in range(n_sigma)]).T.tolist()
     uei = np.asarray(
             [unscented_transform(
                 np.expand_dims(ei, axis=0).T, sigma_gen.Wm, sigma_gen.Wc)[0] \
                     for ei in np.asarray(eis).T])
     if return_grad:
-        # uei = unscented_transform(np.array(eis), sigma_gen.Wm, sigma_gen.Wc)[0]
         ugrad = np.asarray(
             [unscented_transform(
                 np.expand_dims(grad, axis=0).T, sigma_gen.Wm, sigma_gen.Wc)[0] \
                     for grad in np.asarray(grads).T]).T
         return uei.squeeze(), ugrad#.squeeze()
     else:
-        # uei = np.asarray([
-        #     unscented_transform(eis.sexpand_dims(axis=0), sigma_gen.Wm, sigma_gen.Wc)[0] for eis in sigma_ei])
         uei = np.asarray(
             [unscented_transform(
                 np.expand_dims(ei, axis=0).T, sigma_gen.Wm, sigma_gen.Wc)[0] \
