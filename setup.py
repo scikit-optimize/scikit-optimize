b8994a860c72116f2f43ace968c395dcef71a23d
Matthew Feickert <matthew.feickert@cern.ch>
2021-10-04 01:39:22 -0500

    docs: Add Python 3.9 to PyPI metadata

--- setup.py
+++ setup.py
@@ -29,7 +29,8 @@ CLASSIFIERS = ['Intended Audience :: Science/Research',
                'Operating System :: MacOS',
                'Programming Language :: Python :: 3.6',
                'Programming Language :: Python :: 3.7',
-               'Programming Language :: Python :: 3.8']
+               'Programming Language :: Python :: 3.8',
+               'Programming Language :: Python :: 3.9']
 
 
 setup(name='scikit-optimize',
