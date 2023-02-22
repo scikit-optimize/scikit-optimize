52a7db95cb567186fb4e9003139fea4592bdbf05
Jonas Tørnes <jonas.tornes@gmail.com>
2023-02-22 17:03:25 +0100

    Fix line widht issues

--- skopt/learning/forest.py
+++ skopt/learning/forest.py
@@ -390,8 +390,8 @@ class ExtraTreesRegressor(_sk_ExtraTreesRegressor):
     .. [1] L. Breiman, "Random Forests", Machine Learning, 45(1), 5-32, 2001.
 
     """
-    def __init__(self, n_estimators=10, criterion='squared_error', max_depth=None,
-                 min_samples_split=2, min_samples_leaf=1,
+    def __init__(self, n_estimators=10, criterion='squared_error',
+                 max_depth=None, min_samples_split=2, min_samples_leaf=1,
                  min_weight_fraction_leaf=0.0, max_features='auto',
                  max_leaf_nodes=None, min_impurity_decrease=0.,
                  bootstrap=False, oob_score=False,
