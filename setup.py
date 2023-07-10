be58438a3e1bc764ec69ae5bd47067a98f6b7a2e
Mike Smith <mikesmithlab@gmail.com>
2023-07-10 15:26:24 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,16 +6,19 @@ with open("README.md", "r") as fh:
 with open("LICENSE", "r") as fh:
     license = fh.read()
 
+import scikit-optimize.skopt as skopt
+
 setuptools.setup(
     name='scikit-optimize',
     version='0.1',
     license=license,
-    packages=setuptools.find_packages(
-        exclude=('doc')
-    ),
+    packages=['skopt', 'skopt.learning', 'skopt.optimizer', 'skopt.space',
+                'skopt.learning.gaussian_process', 'skopt.sampler'],
+    #packages=setuptools.find_packages(
+    #    exclude=('doc')
+    #),
     url='https://github.com/MikeSmithLab/scikit-optimize',
-    #packages=['skopt', 'skopt.learning', 'skopt.optimizer', 'skopt.space',
-    #            'skopt.learning.gaussian_process', 'skopt.sampler'],
+    
     install_requires=['joblib>=0.11', 'pyaml>=16.9', 'numpy>=1.13.3',
                         'scipy>=0.19.1',
                         'scikit-learn>=0.20.0'],   
