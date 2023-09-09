80a758bf539a2c0e994a96683233b4442da8ed70
c-w-m <craig.william.miller@gmail.com>
2023-09-09 17:01:44 -0600

    lint cleanup

--- skopt/tests/test_space.py
+++ skopt/tests/test_space.py
@@ -175,11 +175,13 @@ def test_categorical_transform_binary():
 @pytest.mark.fast_test
 def test_categorical_repr():
     small_cat = Categorical([1, 2, 3, 4, 5])
-    assert (small_cat.__repr__() == "Categorical(categories=(1, 2, 3, 4, 5), prior=None)")
+    assert (small_cat.__repr__() == 
+            "Categorical(categories=(1, 2, 3, 4, 5), prior=None)")
     # pylint: disable=C2801 unnecessary-dunder-call
 
     big_cat = Categorical([1, 2, 3, 4, 5, 6, 7, 8])
-    assert (big_cat.__repr__() == 'Categorical(categories=(1, 2, 3, ..., 6, 7, 8), prior=None)')
+    assert (big_cat.__repr__() == 
+            'Categorical(categories=(1, 2, 3, ..., 6, 7, 8), prior=None)')
     # pylint: disable=C2801 unnecessary-dunder-call
 
 
